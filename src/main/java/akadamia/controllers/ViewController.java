7d2bb9ec71a3389efad5c95021b67c0601a23885
Lukasz Dusza <lukaszdusza280@gmail.com>
2020-03-21 11:29:26 +0100

    table works

--- src/main/java/akadamia/controllers/ViewController.java
+++ src/main/java/akadamia/controllers/ViewController.java
@@ -1,8 +1,23 @@
 package akadamia.controllers;
 
+import akadamia.services.dto.HotelServiceDTO;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ViewController {
+
+  private HotelServiceDTO hotelServiceDTO;
+
+  public ViewController(HotelServiceDTO hotelServiceDTO) {
+    this.hotelServiceDTO = hotelServiceDTO;
+  }
+
+  @GetMapping("/home")
+  public String getHomePage(Model model) {
+    model.addAttribute("hotels", hotelServiceDTO.getHotels());
+    return "index";
+  }
 
 }
